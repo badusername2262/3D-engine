1e096d0a57c783770a375de83b4610874ed7c4bf
badusername2262 <Badusername2262@gmail.com>
2022-05-29 17:33:24 +1000

    a

---
+++ build/CMakeFiles/engine.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/alexr/eclipse-workspace/3D-engine/src/Buffers/buffer.cpp" "CMakeFiles/engine.dir/src/Buffers/buffer.cpp.obj" "gcc" "CMakeFiles/engine.dir/src/Buffers/buffer.cpp.obj.d"
+  "C:/Users/alexr/eclipse-workspace/3D-engine/src/Buffers/indexbuffer.cpp" "CMakeFiles/engine.dir/src/Buffers/indexbuffer.cpp.obj" "gcc" "CMakeFiles/engine.dir/src/Buffers/indexbuffer.cpp.obj.d"
+  "C:/Users/alexr/eclipse-workspace/3D-engine/src/Buffers/vertexarray.cpp" "CMakeFiles/engine.dir/src/Buffers/vertexarray.cpp.obj" "gcc" "CMakeFiles/engine.dir/src/Buffers/vertexarray.cpp.obj.d"
+  "C:/Users/alexr/eclipse-workspace/3D-engine/src/Graphics/shader.cpp" "CMakeFiles/engine.dir/src/Graphics/shader.cpp.obj" "gcc" "CMakeFiles/engine.dir/src/Graphics/shader.cpp.obj.d"
+  "C:/Users/alexr/eclipse-workspace/3D-engine/src/Graphics/window.cpp" "CMakeFiles/engine.dir/src/Graphics/window.cpp.obj" "gcc" "CMakeFiles/engine.dir/src/Graphics/window.cpp.obj.d"
+  "C:/Users/alexr/eclipse-workspace/3D-engine/src/Utils/camera.cpp" "CMakeFiles/engine.dir/src/Utils/camera.cpp.obj" "gcc" "CMakeFiles/engine.dir/src/Utils/camera.cpp.obj.d"
+  "C:/Users/alexr/eclipse-workspace/3D-engine/src/main.cpp" "CMakeFiles/engine.dir/src/main.cpp.obj" "gcc" "CMakeFiles/engine.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/engine.dir/src/Buffers/indexbuffer.cpp.obj.d
@@ -0,0 +1,16 @@
+CMakeFiles/engine.dir/src/Buffers/indexbuffer.cpp.obj: \
+ C:\Users\alexr\eclipse-workspace\3D-engine\src\Buffers\indexbuffer.cpp \
+ C:\Users\alexr\eclipse-workspace\3D-engine\src\Buffers\indexbuffer.hpp \
+ C:/Users/alexr/eclipse-workspace/3D-engine/include/GL/glew.h \
+ C:/msys64/mingw64/include/inttypes.h C:/msys64/mingw64/include/crtdefs.h \
+ C:/msys64/mingw64/include/corecrt.h C:/msys64/mingw64/include/_mingw.h \
+ C:/msys64/mingw64/include/_mingw_mac.h \
+ C:/msys64/mingw64/include/_mingw_secapi.h \
+ C:/msys64/mingw64/include/vadefs.h \
+ C:/msys64/mingw64/include/sdks/_mingw_ddk.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/12.1.0/include/stdint.h \
+ C:/msys64/mingw64/include/stdint.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/12.1.0/include/stddef.h \
+ C:/msys64/mingw64/include/stddef.h \
+ C:/Users/alexr/eclipse-workspace/3D-engine/include/GL/glu.h \
+ C:/Users/alexr/eclipse-workspace/3D-engine/include/GL/gl.h
